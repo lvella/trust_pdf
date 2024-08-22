4b247d44e03c9b7dd90d66374fa84d35d0259343
Lucas Clemente Vella <lvella@gmail.com>
2024-08-22 01:44:04 +0100

    Quite some progress.

--- src/pdf_validation/increment_validation.rs
+++ src/pdf_validation/increment_validation.rs
@@ -1,11 +1,6 @@
 use super::Signature;
 use lopdf::{xref::XrefEntry, Dictionary, Document, Object, ObjectId};
-use openssl::{stack, x509::verify};
-use std::{
-    cell::RefCell,
-    collections::{hash_map::Entry, BTreeMap, HashMap},
-    vec,
-};
+use std::{cell::RefCell, collections::HashMap};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -16,8 +11,14 @@ pub enum Error {
     CatalogMismatch,
     #[error("mismatch between /AcroForm dictionaries")]
     AcroFormMismatch,
-    #[error("only uncompressed xref entries are supported")]
-    UnsupportedXrefEntry,
+    #[error("array has more than one extra reference")]
+    NotSingleArrayIncrement,
+    #[error("two different signature annotations found in the increment")]
+    TwoDifferentSignatureInIncrement,
+    #[error("multiple pages changed in the increment")]
+    MultiplePagesChanged,
+    #[error("mismatch between /Page dictionaries")]
+    PageMismatch,
     #[error("mismatch between xref tables")]
     XrefMismatch,
 }
@@ -56,7 +57,7 @@ impl<'a> DocTracker<'a> {
         .get_dict_deref(b"Root")
     }
 
-    fn deref(&self, mut obj: &'a Object) -> Result<&Object> {
+    fn deref(&self, obj: &'a Object) -> Result<&Object> {
         // It is reasonable to only track the first reference in a chain of
         // references, because that is the only one that needs to change.
         match obj {
@@ -83,7 +84,7 @@ impl<'a> DocTracker<'a> {
         let traversed = self.traversed.into_inner();
 
         for (id, entry) in self.doc.reference_table.entries.iter() {
-            if let Some(gen) = traversed.get(id) {
+            if traversed.get(id).is_some() {
                 // This entry is allowed to be different.
                 continue;
             }
@@ -147,22 +148,28 @@ pub fn verify_increment(
 
     let prev_doc_tracker = DocTracker::new(previous_doc);
 
-    verify_catalogs(&curr_doc, &prev_doc_tracker)?;
+    verify_catalogs(curr_sig, curr_doc, &prev_doc_tracker)?;
 
     // All the indirect objects in the tracked list are allowed to be
     // different from the corresponding object in the current document.
     // All others must match.
-    prev_doc_tracker.verify_all_changes_are_allowed(&curr_doc)?;
+    prev_doc_tracker.verify_all_changes_are_allowed(curr_doc)?;
 
     Ok(())
 }
 
-fn verify_catalogs<'a, 'b>(curr_doc: &'a Document, previous_doc: &'b DocTracker<'b>) -> Result<()> {
+fn verify_catalogs(
+    curr_sig: &Signature,
+    curr_doc: &Document,
+    previous_doc: &DocTracker,
+) -> Result<()> {
     let curr_catalog = curr_doc.catalog()?;
     let prev_catalog = previous_doc.catalog()?;
 
     let mut prev_acro_form = None;
     let mut size_diff = 1;
+
+    let mut pages = None;
 
     for (key, obj) in prev_catalog.dict.iter() {
         if key == b"AcroForm" {
@@ -174,37 +181,54 @@ fn verify_catalogs<'a, 'b>(curr_doc: &'a Document, previous_doc: &'b DocTracker<
         }
 
         if key == b"Pages" {
-            // Pages object is also handled separately.
-            continue;
+            // The /Type /Pages dictionary must be a reference to a dictionary
+            // and remain the same, but we still need to check the individual
+            // pages, so we store it for later use.
+            pages = Some(curr_doc.get_dictionary(obj.as_reference()?)?);
         }
 
-        let curr_obj = curr_catalog.dict.get(key)?;
+        let curr_obj = curr_catalog.get(key)?;
         if curr_obj != obj {
             return Err(Error::CatalogMismatch);
         }
     }
 
     // All the elements from both catalogs must have matched.
-    if prev_catalog.dict.len() + size_diff != curr_catalog.dict.len() {
+    if prev_catalog.dict.len() + size_diff != curr_catalog.len() {
         return Err(Error::CatalogMismatch);
     }
 
     // Current catalog must have an AcroForm dictionary.
-    verify_acro_forms(curr_catalog.get_dict_deref(b"AcroForm")?, prev_acro_form)?;
+    let new_signature_id_f = verify_acro_forms(
+        curr_doc,
+        curr_doc.get_dict_in_dict(curr_catalog, b"AcroForm")?,
+        prev_acro_form,
+    )?;
 
-    // Pages object must exist in both, catalogs, too.
-    verify_pages(
-        curr_catalog.get_dict_deref(b"Pages")?,
-        prev_catalog.get_dict_deref(b"Pages")?,
-    );
+    // Each page individually can be different (by one annotation, at most), but
+    // the /Type /Pages dictionary must remain the same.
+    let new_signature_id_p = verify_pages(
+        curr_doc,
+        previous_doc,
+        pages.ok_or(Error::Parsing(lopdf::Error::DictKey))?,
+    )?;
+
+    if let Some(new_signature_id_p) = new_signature_id_p {
+        if new_signature_id_f != new_signature_id_p {
+            return Err(Error::TwoDifferentSignatureInIncrement);
+        }
+    }
+
+    verify_sig_annotation(curr_sig, curr_doc, new_signature_id_f)?;
 
     Ok(())
 }
 
 fn verify_acro_forms(
-    curr_acro_form: DictTracker,
+    curr_doc: &Document,
+    curr_acro_form: &Dictionary,
     prev_acro_form: Option<DictTracker>,
-) -> Result<()> {
+) -> Result<ObjectId> {
     let mut prev_has_da = false;
     let mut prev_has_dr = false;
 
@@ -214,7 +238,7 @@ fn verify_acro_forms(
         for (key, obj) in prev_acro_form.dict.iter() {
             if key == b"Fields" {
                 // Fields are handled separately.
-                prev_fields = Some(prev_acro_form.tracker.deref_array(obj)?);
+                prev_fields = Some(prev_acro_form.tracker.deref(obj)?.as_array()?);
                 continue;
             }
 
@@ -224,7 +248,7 @@ fn verify_acro_forms(
                 prev_has_dr = true;
             }
 
-            let curr_obj = curr_acro_form.dict.get(key)?;
+            let curr_obj = curr_acro_form.get(key)?;
             if curr_obj != obj {
                 return Err(Error::AcroFormMismatch);
             }
@@ -234,9 +258,8 @@ fn verify_acro_forms(
         1
     };
 
-    let handle_extra_field = |key| {
-        if let Ok(obj) = curr_acro_form.dict.get(key) {
-            curr_acro_form.tracker.deep_track(obj)?;
+    let mut handle_extra_field = |key| {
+        if curr_acro_form.has(key) {
             allowed_len += 1;
         }
     };
@@ -249,17 +272,127 @@ fn verify_acro_forms(
         handle_extra_field(b"DR");
     }
 
-    if curr_acro_form.dict.len() != allowed_len {
+    if curr_acro_form.len() != allowed_len {
         return Err(Error::AcroFormMismatch);
     }
 
     // Handle the fields
-    let curr_fields = curr_acro_form.get_array_deref(b"Fields")?;
-    verify_fields(curr_fields, prev_fields)?;
+    let curr_fields = curr_acro_form.get_deref(b"Fields", curr_doc)?.as_array()?;
+    let extra_signature_annotation = has_array_one_extra_ref(curr_fields, prev_fields)?;
 
-    Ok(())
+    extra_signature_annotation.ok_or(Error::NotSingleArrayIncrement)
 }
 
-fn verify_pages(curr_pages: DictTracker, prev_pages: DictTracker) {
+/// Gets the single extra ObjectId curr array has compared to the prev array, if any.
+/// Allows for reordering.
+///
+/// Return Ok(None) if the arrays are equial.
+///
+/// Returns or Err in case of any other difference.
+fn has_array_one_extra_ref(
+    curr_refs: &[Object],
+    prev_refs: Option<&Vec<Object>>,
+) -> Result<Option<ObjectId>> {
+    fn sort_array(array: &[Object]) -> Result<Vec<ObjectId>> {
+        let mut array = array
+            .iter()
+            .map(|obj| obj.as_reference())
+            .collect::<lopdf::Result<Vec<ObjectId>>>()?;
+        array.sort();
+        Ok(array)
+    }
+
+    let prev_refs = prev_refs.map_or([].as_slice(), |prev_refs| prev_refs);
+    let prev_refs = sort_array(prev_refs)?;
+    let curr_refs = sort_array(curr_refs)?;
+
+    let mut prev_iter = prev_refs.into_iter().peekable();
+    let mut curr_iter = curr_refs.into_iter().peekable();
+
+    while let (Some(prev), Some(curr)) = (prev_iter.peek(), curr_iter.peek()) {
+        if prev != curr {
+            break;
+        }
+        prev_iter.next();
+        curr_iter.next();
+    }
+
+    let odd_one_out = match (prev_iter.peek(), curr_iter.next()) {
+        (None, None) => None,
+        (_, odd_one_out) => odd_one_out,
+    };
+
+    if curr_iter.eq(prev_iter) {
+        return Err(Error::NotSingleArrayIncrement);
+    }
+
+    Ok(odd_one_out)
+}
+
+fn verify_pages(
+    curr_doc: &Document,
+    prev_doc: &DocTracker,
+    pages: &Dictionary,
+) -> Result<Option<ObjectId>> {
+    let mut extra_annotation = None;
+
+    let kids = pages.get_deref(b"Kids", curr_doc)?.as_array()?;
+    for page in kids {
+        // Ensure the page is a reference.
+        page.as_reference()?;
+
+        if let Some(extra) = verify_page(
+            curr_doc,
+            curr_doc.dereference(page)?.1.as_dict()?,
+            prev_doc.deref_dict(page)?,
+        )? {
+            if extra_annotation.is_some() {
+                return Err(Error::MultiplePagesChanged);
+            }
+            extra_annotation = Some(extra);
+        }
+    }
+
+    Ok(extra_annotation)
+}
+
+fn verify_page(
+    curr_doc: &Document,
+    curr_page: &Dictionary,
+    prev_page: DictTracker,
+) -> Result<Option<ObjectId>> {
+    let mut extra_annotation = None;
+
+    for (key, obj) in prev_page.dict.iter() {
+        if key == b"Annots" {
+            let curr_annots = curr_page.get_deref(b"Annots", curr_doc)?.as_array()?;
+            let prev_annots = prev_page.tracker.deref(obj)?.as_array()?;
+
+            if let Some(annot) = has_array_one_extra_ref(curr_annots, Some(prev_annots))? {
+                if extra_annotation.is_some() {
+                    return Err(Error::PageMismatch);
+                }
+                extra_annotation = Some(annot);
+            }
+        }
+
+        let curr_obj = curr_page.get(key)?;
+        if curr_obj != obj {
+            return Err(Error::PageMismatch);
+        }
+    }
+
+    if curr_page.len() != prev_page.dict.len() {
+        return Err(Error::PageMismatch);
+    }
+
+    Ok(extra_annotation)
+}
+
+fn verify_sig_annotation(
+    curr_sig: &Signature,
+    curr_doc: &Document,
+    sig_id: ObjectId,
+) -> Result<()> {
     todo!()
 }
